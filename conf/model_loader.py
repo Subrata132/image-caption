417675d38a4e696ffe6ff936d1abcfea8c17d5d0
Subrata132 <subrata.biswwas@gmail.com>
2022-04-03 14:00:43 +0600

    removed api.py.

--- conf/model_loader.py
+++ conf/model_loader.py
@@ -3,10 +3,27 @@ import pandas as pd
 from PIL import Image
 import torch
 import torchvision.transforms as T
+import matplotlib.pyplot as plt
 from captionist.utils import load_train_test_val_data
 from captionist.vocab_builder import Vocabulary
 from captionist.data_util import data_loader_
 from captionist.model import EncoderDecoder
+
+
+def save_attention(img_name, result, attention_plot):
+    temp_image = Image.open(f'uploaded_images/{img_name}').convert("RGB")
+    fig = plt.figure(figsize=(10, 10))
+    len_result = len(result)
+    for l in range(len_result):
+        temp_att = attention_plot[l].reshape(8, 8)
+        ax = fig.add_subplot(len_result // 2, len_result // 2, l + 1)
+        ax.set_title(result[l], fontsize=12)
+        img = ax.imshow(temp_image)
+        ax.imshow(temp_att, cmap='gray', alpha=0.7, extent=img.get_extent())
+        ax.set_xticks([], minor=False)
+        ax.set_yticks([], minor=False)
+    plt.tight_layout()
+    plt.savefig(f'attention_images/attention_{img_name.split(".")[0]}.png', dpi=150)
 
 
 def load_model(parameters):
@@ -60,4 +77,9 @@ class Explainer:
             input_img = input_img.unsqueeze(0)
             encoded_image = self.model.image_encoder(input_img.to(self.device))
             caps, alphas = self.model.decoder.generate_caption(encoded_image, vocab=self.vocab)
-            return {'caption': ' '.join(caps[:-1])}
+            save_attention(image_name, caps, alphas)
+            data = {
+                'caption': ' '.join(caps[:-1]),
+                'att_url': f'attention_images/attention_{image_name.split(".")[0]}.png'
+            }
+            return data
